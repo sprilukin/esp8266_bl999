beaf1a7c6785d3fda463e6512b984bd6471dc607
Sergey Prilukin <sprilukin@gmail.com>
2016-01-03 17:28:57 +0200

    in progress

--- esp8266_bl999.ino
+++ esp8266_bl999.ino
@@ -7,12 +7,14 @@
 
 //Thingspeak setup
 #define thingspeakApiKey ".................."
-char thingSpeakAddress[] = "api.thingspeak.com";
+#define thingSpeakAddress "api.thingspeak.com"
+#define thingSpeakUpdateJsonEndpoint "/update.json"
 #define thingSpeakHttpPort 80
 #define thingSpeakUpdateInterval 16000 //16 sec
 
 //bl999 setup
 static BL999Info info;
+#define bl999_pin 4
 
 //wifi client
 WiFiClient client;
@@ -21,7 +23,7 @@ void setup() {
     Serial.begin(115200);
 
     //set digital pin to read info from
-    bl999_set_rx_pin(4);
+    bl999_set_rx_pin(bl999_pin);
 
     //start reading data from sensor
     bl999_rx_start();
@@ -48,12 +50,11 @@ void loop() {
     //read message to info and if check sum correct - output it to serial port
     if (bl999_get_message(info)) {
         Serial.println("====== Got message: ");
-        Serial.println(info.channel);
-        Serial.println(info.powerUUID);
-        Serial.println(info.battery);
-        Serial.println(info.temperature);
-        Serial.println(info.humidity);
-        //postData(info);
+        Serial.println("Channel: " + String(info.channel));
+        Serial.println("powerUUID: " + String(info.powerUUID));
+        Serial.println("battery: " + String(info.battery == 0));
+        Serial.println("temperature: " + String(info.temperature / 10.0));
+        Serial.println("humidity: " + String(info.humidity));
 
 
         if (!client.connect(thingSpeakAddress, thingSpeakHttpPort)) {
@@ -63,10 +64,10 @@ void loop() {
 
         String postString = "field1="+String(info.channel)+"&field2="+String(info.powerUUID)+"&field3="+String(info.battery)+"&field4="+String(info.temperature/10.0)+"&field5="+String(info.humidity);
 
-        client.println("POST /update.json HTTP/1.1");
+        client.println("POST " + String(thingSpeakUpdateJsonEndpoint) + " HTTP/1.1");
         client.println("Host: " + String(thingSpeakAddress));
         client.println("Connection: close");
-        client.println("X-THINGSPEAKAPIKEY: "+String(thingspeakApiKey));
+        client.println("X-THINGSPEAKAPIKEY: " + String(thingspeakApiKey));
         client.println("Content-Type: application/x-www-form-urlencoded");
         client.print("Content-Length: " + String(postString.length()));
         client.print("\r\n\r\n");
@@ -88,4 +89,7 @@ void loop() {
 void postData(BL999Info& info) {
 
 }
+
+
+
 
