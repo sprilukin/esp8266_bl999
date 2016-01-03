b9c719f1e45b2cd51a1ba6e63e98855fab13e0dd
Sergey Prilukin <sprilukin@gmail.com>
2016-01-03 17:12:39 +0200

    initial commit

---
+++ esp8266_bl999.ino
@@ -0,0 +1,91 @@
+#include <ESP8266WiFi.h>
+#include <lib_bl999.h>
+
+//WiFi setup
+#define ssid       "............"
+#define password   "..........."
+
+//Thingspeak setup
+#define thingspeakApiKey ".................."
+char thingSpeakAddress[] = "api.thingspeak.com";
+#define thingSpeakHttpPort 80
+#define thingSpeakUpdateInterval 16000 //16 sec
+
+//bl999 setup
+static BL999Info info;
+
+//wifi client
+WiFiClient client;
+
+void setup() {
+    Serial.begin(115200);
+
+    //set digital pin to read info from
+    bl999_set_rx_pin(4);
+
+    //start reading data from sensor
+    bl999_rx_start();
+
+    //setup wifi
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+    //blocks until message will not be read
+    bl999_wait_rx();
+
+    //read message to info and if check sum correct - output it to serial port
+    if (bl999_get_message(info)) {
+        Serial.println("====== Got message: ");
+        Serial.println(info.channel);
+        Serial.println(info.powerUUID);
+        Serial.println(info.battery);
+        Serial.println(info.temperature);
+        Serial.println(info.humidity);
+        //postData(info);
+
+
+        if (!client.connect(thingSpeakAddress, thingSpeakHttpPort)) {
+            Serial.println("connection failed");
+            return;
+        }
+
+        String postString = "field1="+String(info.channel)+"&field2="+String(info.powerUUID)+"&field3="+String(info.battery)+"&field4="+String(info.temperature/10.0)+"&field5="+String(info.humidity);
+
+        client.println("POST /update.json HTTP/1.1");
+        client.println("Host: " + String(thingSpeakAddress));
+        client.println("Connection: close");
+        client.println("X-THINGSPEAKAPIKEY: "+String(thingspeakApiKey));
+        client.println("Content-Type: application/x-www-form-urlencoded");
+        client.print("Content-Length: " + String(postString.length()));
+        client.print("\r\n\r\n");
+        client.print(postString);
+
+        delay(500);
+
+        // Read all the lines of the reply from server and print them to Serial
+        while(client.available()){
+            String line = client.readStringUntil('\r');
+            //Serial.print(line);
+        }
+
+        Serial.println();
+        Serial.println("closing connection");
+    }
+}
+
+void postData(BL999Info& info) {
+
+}
+
